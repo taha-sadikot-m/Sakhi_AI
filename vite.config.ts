609ab9db6531d368a16d878af21a8cf6664191ad
Taha Sadikot <taha.sadikot.m@gmail.com>
2025-05-28 13:25:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => {
       server: {
         host: '0.0.0.0', // Ensure the server binds to 0.0.0.0
         port: 3000,
+        allowedHosts: ['*']
       }
     };
 });
